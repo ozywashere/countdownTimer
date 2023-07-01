9ffb12b46d6657d838f8e3b0d37969590ded35b7
ozywashere <durduarslantas@ogr.iu.edu.tr>
2023-07-01 22:51:06 +0300

    countdown timer created

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+
+const weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
+
+const items = document.querySelectorAll('.countdown__item-number')
+
+const futureDate = new Date(2024, 0, 0, 0)
+const futureTime = futureDate.getTime()
+
+function countdown() {
+  const today = new Date().getTime()
+  const t = futureTime - today
+
+  const oneDay = 24 * 60 * 60 * 1000
+  const oneHour = 60 * 60 * 1000
+  const oneMinute = 60 * 1000
+  const oneSecond = 1000
+
+  let days = Math.floor(t / oneDay)
+  let hours = Math.floor((t % oneDay) / oneHour)
+  let minutes = Math.floor((t % oneHour) / oneMinute)
+  let seconds = Math.floor((t % oneMinute) / oneSecond)
+  const values = [days, hours, minutes, seconds]
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`)
+    }
+    return item
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index])
+  })
+}
+
+let countdownInterval = setInterval(countdown, 1000)
+
+countdown()
